54205b274c1ff625c14c782fda1a9d23b7ceb353
Rita <margvlahou@gmail.com>
2022-11-24 19:36:57 +0000

    add functionality

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+const tabContent = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
   // hide all tab panels
+  tabContent.forEach((panel) => {
+    panel.hidden = true;
+  });
   // unselect all tabs :  tab.ariaSelected = false;
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
   // select the clicked tab
-  // display the relevaly tab
+  event.currentTarget.setAttribute('aria-selected', true);
+  // display the relevalt tab
+  const { id } = event.currentTarget;
+
+  // find in the array of tabContent
+  console.log(tabContent);
+  const tabPanel = tabContent.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
 }
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);
