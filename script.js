367bef3ff9404ab7a4aadf3735c1d4298b2d8454
Rita <margvlahou@gmail.com>
2022-11-24 19:24:35 +0000

    add handleTabClickFunction

---
+++ script.js
@@ -0,0 +1,10 @@
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // hide all tab panels
+  // unselect all tabs :  tab.ariaSelected = false;
+  // select the clicked tab
+  // display the relevaly tab
+}
