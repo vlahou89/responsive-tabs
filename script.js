bef232ffb293c38d780004b1132ff244929b00d2
Rita <margvlahou@gmail.com>
2022-12-09 19:45:14 +0000

    add keyboard functionality

--- script.js
+++ script.js
@@ -1,29 +1,110 @@
-const tabs = document.querySelector('.tabs');
-const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabContent = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+const TABLIST = document.querySelector('#tablist');
+const TABS = [...TABLIST.querySelectorAll('.tab')];
+const TABPANELS = [...document.querySelectorAll('.tabpanel')];
 
-function handleTabClick(event) {
-  // hide all tab panels
-  tabContent.forEach((panel) => {
-    panel.hidden = true;
+// Hide all panels and make visible only the selected one by using the aria-labelledby
+//and selectedId
+const showActivePanel = (element) => {
+  const selectedId = element.id;
+  TABPANELS.forEach((e) => {
+    e.hidden = 'true';
   });
-  // unselect all tabs :  tab.ariaSelected = false;
-  tabButtons.forEach((tab) => {
-    tab.setAttribute('aria-selected', false);
-  });
-  // select the clicked tab
-  event.currentTarget.setAttribute('aria-selected', true);
-  // display the relevalt tab
-  const { id } = event.currentTarget;
-
-  // find in the array of tabContent
-  console.log(tabContent);
-  const tabPanel = tabContent.find(
-    (panel) => panel.getAttribute('aria-labelledby') === id
+  const activePanel = document.querySelector(
+    `[aria-labelledby="${selectedId}"]`
   );
-  tabPanel.hidden = false;
-}
+  activePanel.removeAttribute('hidden');
+};
 
-tabButtons.forEach((button) =>
-  button.addEventListener('click', handleTabClick)
-);
+// set the new selected tab and deselect the first tab which was selected as default
+// loop through the tabs and check which one if the selected id is the current id
+//if yes -> set aria-selected to true and make it focusable
+const setSelectedTab = (element) => {
+  const selectedId = element.id;
+  TABPANELS[0].classList.remove('visible');
+  TABS.forEach((e) => {
+    const id = e.getAttribute('id');
+    if (id === selectedId) {
+      e.removeAttribute('tabindex', '0');
+      e.setAttribute('aria-selected', 'true');
+    } else {
+      e.setAttribute('tabindex', '-1');
+      e.setAttribute('aria-selected', 'false');
+    }
+  });
+};
+
+// Add the keyboard navigation to the
+// ArrowUp & Arrow left focus on the precious tab
+// ArrowDown & ArrowRight focus on the next tab
+const createArrowNavigation = () => {
+  const firstTab = TABS[0];
+  const lastTab = TABS[TABS.length - 1];
+
+  TABS.forEach((element) => {
+    element.addEventListener('keydown', function (e) {
+      if (e.key === 'ArrowUp' || e.key === 'ArrowLeft') {
+        if (element == firstTab) {
+          e.preventDefault();
+          lastTab.focus();
+        } else {
+          e.preventDefault();
+          const focusableElement = TABS.indexOf(element) - 1;
+          TABS[focusableElement].focus();
+        }
+      } else if (e.key === 'ArrowDown' || e.key === 'ArrowRight') {
+        if (element == lastTab) {
+          e.preventDefault();
+          firstTab.focus();
+        } else {
+          e.preventDefault();
+          const focusableElement = TABS.indexOf(element) + 1;
+          TABS[focusableElement].focus();
+        }
+      }
+    });
+  });
+};
+
+// On  click set the selected tab and the selected panel
+const handleClick = () => {
+  TABS.forEach((element) => {
+    element.addEventListener('click', function () {
+      setSelectedTab(element);
+      showActivePanel(element);
+    });
+  });
+};
+
+handleClick();
+createArrowNavigation();
+
+// Initialize tabs
+
+//Initialize tabs on load
+const activateFirstPanel = () => {
+  TABS[0].setAttribute('tabindex', '0');
+  TABS[0].setAttribute('aria-selected', 'true');
+  TABPANELS[0].classList.add('visible');
+};
+
+const checkInitialSelectedTab = () => {
+  const targetedTabPanel = document
+    .querySelector('.tabpanel:target')
+    .getAttribute('aria-labelledby');
+  const selectedTab = document.querySelector(`#${targetedTabPanel}`);
+  selectedTab.setAttribute('aria-selected', 'true');
+  selectedTab.removeAttribute('tabindex');
+};
+
+const handleInitialState = () => {
+  TABS.forEach((e) => {
+    e.setAttribute('tabindex', '-1');
+    e.setAttribute('aria-selected', 'false');
+  });
+
+  window.location.href.indexOf('#panel') === -1
+    ? activateFirstPanel()
+    : checkInitialSelectedTab();
+};
+
+window.onload = handleInitialState;
